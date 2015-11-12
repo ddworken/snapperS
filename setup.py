be6a11450e6548e558330ec78dd84252be940fcf
David Dworken <david@daviddworken.com>
2015-11-12 11:07:27 -0500

    Added --verbose flag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='snapperS',
-    version='0.1.4',
+    version='0.1.5',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['snapperS'],
