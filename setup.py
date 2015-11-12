af12f596f1f6e36a8ecbd4599e3ee5257ef8090c
David Dworken <david@daviddworken.com>
2015-11-12 09:07:39 -0500

    Fixed bug with hardcoded /.snapshots/ directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='snapperS',
-    version='0.1.2',
+    version='0.1.3',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['snapperS'],
