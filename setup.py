e0bdb72da2cceed6a5fb0842efc1b4eef06d84b3
David Dworken <david@daviddworken.com>
2015-11-11 21:13:03 -0500

    Upload to pypi

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+    name='snapperS',
+    version='0.1.0',
+    author='David Dworken',
+    author_email='david@daviddworken.com',
+    packages=['snapperS'],
+    url='http://pypi.python.org/pypi/snapperS/',
+    license='LICENSE.txt',
+    description='A set of supplementary commands to be used with snapper',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "argparse",
+        "subprocess",
+    ],
+)
+
