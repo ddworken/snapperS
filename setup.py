a8ab3b72071071a7244351d3b938dc8f767b69a7
David Dworken <david@daviddworken.com>
2015-11-11 21:35:43 -0500

    Fixed argument parsing and increased portability

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 
 setup(
     name='snapperS',
-    version='0.1.0',
+    version='0.1.1',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['snapperS'],
     url='http://pypi.python.org/pypi/snapperS/',
     license='LICENSE.txt',
     description='A set of supplementary commands to be used with snapper',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     install_requires=[
         "argparse",
         "subprocess",
