cb5b90d5203c6c04464317be7374bb50a9132314
David Dworken <david@daviddworken.com>
2015-11-13 13:58:46 -0500

    Added specific information on tested versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='snapperS',
-    version='0.3.2',
+    version='0.3.3',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['snapperS'],
