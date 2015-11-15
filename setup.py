11fe5c6fa8dbc49eeafb2293ece69628886d0c38
David Dworken <david@daviddworken.com>
2015-11-15 12:09:16 -0500

    Added --version command

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='snapperS',
-    version='1.0.1',
+    version='1.1.0',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['snapperS'],
