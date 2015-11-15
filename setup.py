22efac984467f7178905bde1659b52644e866cbc
David Dworken <david@daviddworken.com>
2015-11-15 11:42:00 -0500

    Added documentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='snapperS',
-    version='1.0.0',
+    version='1.0.1',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['snapperS'],
