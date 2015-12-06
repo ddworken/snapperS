ccb6a8500153f6bf96131718636d8ed977bc8b4a
David Dworken <david@daviddworken.com>
2015-12-06 15:13:52 -0500

    Added better error handling

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='snapperS',
-    version='1.1.7',
+    version='1.1.8',
     author='David Dworken',
     author_email='david@daviddworken.com',
     packages=['snapperS'],
